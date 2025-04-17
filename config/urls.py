a34ec7364175f8a354ce338ffd39ed0f414019f7
Mehdi Radfar <81019265+funlifew@users.noreply.github.com>
2025-04-18 02:07:04 +0330

    Config - 🍏 Updated urls for media

--- config/urls.py
+++ config/urls.py
@@ -23,3 +23,9 @@ urlpatterns = [
     path("api/push/", include("server.urls")),
     path("webpush/", include("webpush.urls")),
 ]
+
+
+from django.conf import settings
+from django.conf.urls.static import static
+if settings.DEBUG:
+    urlpatterns+=static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
